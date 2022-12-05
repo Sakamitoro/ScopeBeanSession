4cda008e136d8856d05ba69f000b224372f81080
N <Nescafe.gold.1988@mail.ru>
2022-12-05 14:36:12 +0300

    trial version of the session scope sample

---
+++ src/main/java/com/demekhin/ScopeBeanSession/ScopeBeanSessionApplication.java
@@ -0,0 +1,13 @@
+package com.demekhin.ScopeBeanSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopeBeanSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopeBeanSessionApplication.class, args);
+	}
+
+}
